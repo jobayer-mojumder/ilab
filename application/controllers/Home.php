e9ed1a232d6e47e27f351b0ba59fade440e0c87d
jobayermojumder <jobayer.pro@gmail.com>
2019-02-26 08:31:35 +0600

    email sending

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -100,6 +100,8 @@ class Home extends CI_Controller {
 				$this->session->set_flashdata('smsg', 'Please check your email.');
 				redirect('home/details/'.$id);
 			} else {
+				echo $this->email->print_debugger();
+				exit();
 				$this->session->set_flashdata('emsg', 'E-mail Does Not Sent');
 				redirect('home/details/'.$id);
 			}
