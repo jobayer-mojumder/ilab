84af2e34fd0b06a8c274e79d48f65e23d9d6c469
jobayermojumder <jobayer.pro@gmail.com>
2019-02-26 09:19:55 +0600

    email sending

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -74,24 +74,10 @@ class Home extends CI_Controller {
 			</body>
 			</html>";
 
-
-			$config = Array(
-    'protocol' => 'smtp',
-    'smtp_host' => 'ssl://smtp.googlemail.com',
-    'smtp_port' => 465,
-    'smtp_user' => 'xxx',
-    'smtp_pass' => 'xxx',
-    'mailtype'  => 'html', 
-    'charset'   => 'iso-8859-1'
-);
-$this->load->library('email', $config);
-$this->email->set_newline("\r\n");
-
-			
 			$config = array(
 				'protocol'  => 'smtp',
-				'smtp_host' => 'ssl://smtp.googlemail.com',
-				'smtp_port' => 587,
+				'smtp_host' => 'ssl://smtp.gmail.com',
+				'smtp_port' => 465,
 				'smtp_user' => 'ekshop.a2i@gmail.com',
 				'smtp_pass' => 'jiisunseerat',
 				'mailtype'  => 'html',
